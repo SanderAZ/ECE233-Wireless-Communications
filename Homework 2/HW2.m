02b7a4f7c708e749ca860935d648a9e4dbb56785
Sander <44069180+SanderAZ@users.noreply.github.com>
2022-05-27 21:10:04 -0700

    Complete Problem 1 Question 1

--- Homework 2/HW2.m
+++ Homework 2/HW2.m
@@ -1,4 +1,38 @@
-clear all
+clear
+% PROBLEM 1.1
+N_1 = [16, 64];                                                             %Number of Antennae
+K = [1, 2, 4];                                                              %Resolution in bits
+theta = -1*pi:0.01:pi;                                                      %Angles that we will use to compute beamforming gains
+
+for n=1:length(N_1)
+    args = mod(-pi*sin(deg2rad(30))*(0:N_1(n)-1), -2*pi);                   %Phase shifts
+    W = exp(pi*args);
+    figure
+    for k=1:length(K)
+        res = (0:(2^K(k))-1)*((-2*pi)/(2*K(k)));                            %Resolutions
+        args_quantized = interp1(res, res, args, 'nearest', 'extrap');      %Quantize phase shifts
+        Wk = exp(pi*args_quantized);
+        gain = zeros(1,length(theta));
+        for t = 1:length(theta)
+            a_theta = exp(-1j*pi*(0:N_1(n)-1)'*sin(theta(t)))/sqrt(N_1(n)); %ULA array response 
+            gain(t) = abs(Wk*a_theta)^2;                                    %Array gain
+        end
+        plot(theta, gain, 'LineWidth', 1)
+        hold on
+    end
+    % Now do infinite resolution
+    gain = zeros(1,length(theta));
+    for t = 1:length(theta)
+        a_theta = exp(-1j*pi*(0:N_1(n)-1)'*sin(theta(t)))/sqrt(N_1(n));     %ULA array response 
+        gain(t) = abs(W*a_theta)^2;                                         %Array gain
+    end
+    plot(theta, gain, 'LineWidth', 1)
+    legend('k=1', 'k=2', 'k=4', 'Infinite resolution')
+    xlabel('Angle (radians)')
+    ylabel('Array Gain')
+end
+
+
 % % PROBLEM 2
 % % Input given parameters
 % Nt = 8;
@@ -18,7 +52,7 @@ clear all
 % 
 %     % Generate arrays as defined by (4) and (5) of the homework specs
 %     transmitters = 0:1:Nt-1;
-%     arx = arrayfun(@(L)srv(L, theta_i, Nt), transmitters);
+%     arx = arrayfun(@(L)srv(L, theta_i, Nt), transmitters); %srv defined at the bottom
 %     receivers = 0:1:Nr-1;
 %     atx = arrayfun(@(L)srv(L, phi_i, Nt), receivers);
 %     arx = transpose(arx);
@@ -32,56 +66,57 @@ clear all
 % 
 % H_s = coeff * sig;
 
-% PROBLEM 3
-% Generate random 4-QAM symbols
-bits1=randi([0 1],2,1000);
-s1=qammod(bits1, 4, 'InputType', 'bit', 'UnitAveragePower', true);
-bits2 = zeros(2,1000);
-s2=qammod(bits2, 4, 'InputType', 'bit', 'UnitAveragePower', true);
-
-% Upsample by a factor of 4
-s1u = upsample(s1, 4);
-s2u = upsample(s2, 4);
-
-% Create a rcos filter using given parameters
-myfilter = rcosdesign(0.5, 8, 4);
-
-% Apply filter to each signal
-s1t = filter(myfilter, 1, s1u);
-s2t = filter(myfilter, 1, s2u);
-
-% Convert given anges to radians
-phi1 = deg2rad(30);
-phi2 = deg2rad(40);
-
-% Create channels h1 and h2, with # of antennae N
-N = 32;
-antennae = 0:1:N-1;
-h1 = arrayfun(@(L)exp(L*-1i*pi*sin(phi1)), antennae);
-h2 = arrayfun(@(L)exp(L*-1i*pi*sin(phi2)), antennae);
-
-% Apply precoding
-H = [h1.' h2.'].';
-Hherm = H';
-P = Hherm/(H*Hherm);
-st = [s1t.',s2t.'].';
-x = P*st;
-
-% Adjust betas for each section
-beta_1 = 1;
-beta_3 = -133;
-
-% Calculate z(t) as given in equation (10)
-for i = 1:8
-    z(i,:) = beta_1*x(i,:) + beta_3.*x(i,:)*(abs(x(i,:)))'.^2;
-end
-
-% Graph PST of z(1)
-% pwelch(z(1,:), [], [], [], 'mean', 'centered');
-
-
- a_phi = exp(-1i.*pi.*((0:N1-1)-1).*sin(phi_radian(i))).';
- g_phi(1,i) =mean(abs(a_phi'*zn).^2);
+% % PROBLEM 3
+% % Generate random 4-QAM symbols
+% bits1=randi([0 1],2,1000);
+% s1=qammod(bits1, 4, 'InputType', 'bit', 'UnitAveragePower', true);
+% bits2 = zeros(2,1000);
+% s2=qammod(bits2, 4, 'InputType', 'bit', 'UnitAveragePower', true);
+% 
+% % Upsample by a factor of 4
+% s1u = upsample(s1, 4);
+% s2u = upsample(s2, 4);
+% 
+% % Create a rcos filter using given parameters
+% myfilter = rcosdesign(0.5, 8, 4);
+% 
+% % Apply filter to each signal
+% s1t = filter(myfilter, 1, s1u);
+% s2t = filter(myfilter, 1, s2u);
+% 
+% % Convert given anges to radians
+% phi1 = deg2rad(30);
+% phi2 = deg2rad(40);
+% 
+% % Create channels h1 and h2, with # of antennae N
+% N = 32;
+% antennae = 0:1:N-1;
+% h1 = arrayfun(@(L)exp(L*-1i*pi*sin(phi1)), antennae);
+% h2 = arrayfun(@(L)exp(L*-1i*pi*sin(phi2)), antennae);
+% 
+% % Apply precoding
+% H = [h1.' h2.'].';
+% Hherm = H';
+% P = Hherm/(H*Hherm);
+% st = [s1t.',s2t.'].';
+% x = P*st;
+% 
+% % Adjust betas for each section
+% beta_1 = 1;
+% beta_3 = -133;
+% 
+% % Calculate z(t) as given in equation (10)
+% for i = 1:8
+%     z(i,:) = beta_1*x(i,:) + beta_3.*x(i,:)*(abs(x(i,:)))'.^2;
+% end
+% 
+% % Graph PST of z(1)
+% % figure
+% % pwelch(z(1,:), [], [], [], 'mean', 'centered');
+% 
+% 
+%  a_phi = exp(-1i.*pi.*((0:N1-1)-1).*sin(phi_radian(i))).';
+%  g_phi(1,i) =mean(abs(a_phi'*zn).^2);
 
 
 function srv = srv(num, ang, ants)

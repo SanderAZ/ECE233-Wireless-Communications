a5b81ced74b2a4cce635f86e76a4ee0eecc6e8ad
Sander <44069180+SanderAZ@users.noreply.github.com>
2022-06-10 03:23:28 -0700

    Continue working on V-BLAST

--- Project/Zuckerman_Sander_Project.m
+++ Project/Zuckerman_Sander_Project.m
@@ -3,10 +3,11 @@ clc
 close all;
 N = [4 8];          % No. of Receivers = No. of Transmitters; we'll consider 4 of each and 8 of each
 SNR = -20:1:20;     % SNR will range from -20 to 20 dB
-iter = 5000;        % Set the number of Monte-Carlo iterations
+iter = 1000;        % Set the number of Monte-Carlo iterations
 [avgBER_ZF, avgBER_VBLAST, avgBER_ML] = deal(zeros(size(SNR)));   %Initialize
 qpskmod = comm.QPSKModulator;
 qpskdemod = comm.QPSKDemodulator;
+qpskquads = [0.707106781186548 + 0.707106781186548i 0.707106781186548 - 0.707106781186548i -0.707106781186548 + 0.707106781186548i -0.707106781186548 - 0.707106781186548i];
 
 figure
 tic()
@@ -15,6 +16,15 @@ for n = 1:length(N)                                             % Loop through a
         SNR_lin = 10^(SNR(s)/10);                               % Convert SNR to linear scale
         H = 1/sqrt(2)*(rand(N(n), N(n)) +1i*rand(N(n),N(n)));   % Generate channel H
         [iterBER_ZF, iterBER_VBLAST, iterBER_ML] = deal(zeros(size(1, iter)));
+
+        % Setup for ML, will be used later
+        all = dec2bin(0:(2^(N(n)))-1)' - '0';
+        qpskall = zeros(size(all));
+        for a = 1:length(all)
+            foo = qpskmod(all(:,a));
+            qpskall(:,a) = foo;
+        end
+          
         for m = 1:iter
 %             % This is a crude "progress bar", prints status every 1000 iterations
 %             if mod(i,1000) == 0
@@ -23,57 +33,63 @@ for n = 1:length(N)                                             % Loop through a
 
             b_in = randi([0 1],N(n),1);                                             % Generate random signal
             x = qpskmod(b_in);                                                      % Shape signal to QPSK symbol
-            noise = sqrt(1/(2*SNR_lin)) * (randn(N(n),1) + 1j*randn(N(n),1)/N(n));  % Generate complex noise samples with noise power = 1/SNR_lin
+            noise = sqrt(1/(2*SNR_lin)) * (randn(N(n),1) + 1j*randn(N(n),1))/N(n);  % Generate complex noise samples with noise power = 1/SNR_lin
             % Maybe divide by N here?
             y = H*x+noise;                                                          % Received signal
 
-            % Zero-Forcing (ZF)
-            G_ZF = inv(H'*H)*H';
-            x_ZF = G_ZF*y;
-            bOut_ZF = qpskdemod(x_ZF);
-            iterBER_ZF(m) = sum(bOut_ZF ~= b_in)/length(b_in);
+%             % Zero-Forcing (ZF)
+%             G_ZF = inv(H'*H)*H';
+%             x_ZF = G_ZF*y;
+%             bOut_ZF = qpskdemod(x_ZF);
+%             iterBER_ZF(m) = sum(bOut_ZF ~= b_in)/length(b_in);
 
             % V-BLAST
-            G = G_ZF;  % Use H^+ from ZF, will have to calculate on its own when done alone
-            % G(:,:,2) = [0 0 0 0; 0 0 0 0; 0 0 0 0; 0 0 0 0]; A(:,:,x) is page x
-            i = 1;
+            G = inv(H'*H)*H';
+            % A(:,:,x) is page x
+            [Gj, k] = deal(zeros([1, length(G)]));
             for j = 1:length(G)
                 row = G(j,:,1);
-                G1j(j) = norm(row); % find ||(G1)j||^2
+                Gj(j) = norm(row); % find ||(G1)j||^2
             end
-            [~,k(1)] = min(G1j(:));    % find the minimum ||(G1)j||^2 for all j
+            [~,k(1)] = min(Gj(:));    % find the minimum ||(G1)j||^2 for all j
+            r = y;
+            [w, alpha] = deal(zeros([length(G(:,:,1)), length(G(:,:,1))]));
             for K = 1:length(H)
                 w(k(K),:) = G(k(K),:,K);
-                r(k(K),:) = w(k(K),:)'*y(K);
-                foo = r(k(K),:);
-                alpha(k(K),:) = qpskdemod(reshape(foo, [length(foo), 1]));
-                y(i+1) = y(i) - alpha(K(k))*H(K,:);
-                G(:,:,K+1) = G_ZF;
-                G(k(K),:,K+1) = zeros(k(K), 1);
-                for j = 1:length(G)
+                bar = reshape(w(k(K),:), [1, length(w(k(K),:))]);
+                wye(k(K)) = bar*r(:,K); % NEED TO FIX THIS
+%                 foo = wye(k(K),:);
+%                 alpha(k(K),:) = qpskdemod(reshape(foo, [length(foo), 1]));
+                for q = 1:length(qpskquads)
+                    dist(q) = abs(norm(wye(k(K))-qpskquads(q)));
+                end
+                [~,loc] = min(dist(q));
+                alpha(k(K)) = qpskquads(loc);
+                r(:,K+1) = r(:,K) - alpha(k(K),:)*H(:,k(K));
+                G(:,:,K+1) = G(:,:,K);
+                G(:,k(K),K+1) = 0;
+                Gj = zeros(1, length(G));
+                for j = 1:length(G(:,:,K+1))
                     if ~ismember(j, k)
-                        row = G(j,:,1);
-                        G1j(j) = norm(row); % find ||(G1)j||^2
+                        row = G(j,:,K+1);
+                        Gj(j) = norm(row); % find ||(G1)j||^2
+                    else
+                        Gj(j) = realmax;
                     end
                 end
-                [~,k(K+1)] = min(G1j(:));    % find the minimum ||(G1)j||^2 for all j
-                i = i + 1;
+                [~,k(K+1)] = min(Gj(:));    % find the minimum ||(G1)j||^2 for all j
             end
+            bOut_VBLAST = qpskdemod(r(:,end));
+            iterBER_VBLAST(m) = sum(bOut_VBLAST ~= b_in)/length(b_in);
 
 
-            % Maximum Likelihood (ML)
-            all = dec2bin(0:(2^(N(n)))-1)' - '0';
-            qpskall = zeros(size(all));
-            for a = 1:length(all)
-                foo = qpskmod(all(:,a));
-                qpskall(:,a) = foo;
-            end
-            for i = 1:length(qpskall)
-                lengths(i) = norm(y-H*qpskall(:,i))^2;
-            end
-            [val,xhat] = min(lengths);
-            bOut_ML = qpskdemod(qpskall(:,xhat));
-            iterBER_ML(m) = sum(bOut_ML ~= b_in)/length(b_in);
+%             % Maximum Likelihood (ML)
+%             for i = 1:length(qpskall)
+%                 lengths(i) = norm(y-H*qpskall(:,i))^2;
+%             end
+%             [val,xhat] = min(lengths);
+%             bOut_ML = qpskdemod(qpskall(:,xhat));
+%             iterBER_ML(m) = sum(bOut_ML ~= b_in)/length(b_in);
 
         end
         avgBER_ZF(s) = mean(iterBER_ZF);
